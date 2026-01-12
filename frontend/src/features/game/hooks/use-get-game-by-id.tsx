883957b39538640220f576960ac2a5e9354771cf
Victor Sanchez <weebtor.dev@gmail.com>
2026-01-12 20:42:13 -0300

    feat: added past game detail

--- frontend/src/features/game/hooks/use-get-game-by-id.tsx
+++ frontend/src/features/game/hooks/use-get-game-by-id.tsx
@@ -1,14 +1,13 @@
 import apiClient from "@/common/config/api/api-client.config";
 import { useQuery } from "@tanstack/react-query";
-import type { GameModel } from "../types/game.types";
-import type { PublicCardModel } from "../types/card.types";
 import type { GameBoard } from "../types/game.types";
 
-export const useGetGameById = (id: string) => {
+export const useGetGameById = (id?: string) => {
   return useQuery({
+    enabled: !!id,
     queryKey: ["get-game", id],
     queryFn: async () => {
-      return apiClient.get<GameBoard>(`/game/${id}`).then((res) => res.data);
+      return apiClient.get<GameBoard>(`/stats/${id}`).then((res) => res.data);
     },
   });
 };
--- frontend/src/routes/(protected)/app/game/$id.tsx
+++ frontend/src/routes/(protected)/app/game/$id.tsx
@@ -1,15 +1,24 @@
-import { createFileRoute, useParams } from "@tanstack/react-router";
+import ContainerBox from "@/components/ui/container-box";
+import LoadingScreen from "@/features/auth/components/loading-screen";
+import GameBoard from "@/features/game/components/game-board";
+import { useGetGameById } from "@/features/game/hooks/use-get-game-by-id";
+import { createFileRoute, Navigate, useParams } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(protected)/app/game/$id")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const data = useParams({ strict: false });
+  const { id } = useParams({ strict: false });
+  const { data, isFetching } = useGetGameById(id);
+
+  if (isFetching || !id) return <LoadingScreen />;
+
+  if (!data) return <Navigate to="/app/stats" />;
+
   return (
-    <div>
-      Hello "/(protected)/app/game/$id"!
-      {JSON.stringify(data)}
-    </div>
+    <ContainerBox className="flex flex-1 justify-center items-center">
+      <GameBoard {...data} />
+    </ContainerBox>
   );
 }
